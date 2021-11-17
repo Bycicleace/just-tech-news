cd9f0fd0bec4d2fed5b6ee569b1b7a83e98f0748
kvammee <elliott.kvamme@gmail.com>
2021-11-16 19:58:24 -0600

    Added Comments to endpoints

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models/index.js');
+const { Post, User, Vote, Comment } = require('../../models/index.js');
 const sequelize = require('../../config/connection.js');
 
 router.get('/', (req, res) => {
@@ -17,6 +17,14 @@ router.get('/', (req, res) => {
         ],
         order: [['created_at', 'DESC']],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -46,6 +54,14 @@ router.get('/:id', (req, res) => {
             ]
         ],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'user_id', 'post_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
