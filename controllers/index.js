9138675c823edf150acb8dee848167ae4b962402
kvammee <elliott.kvamme@gmail.com>
2021-11-17 14:56:23 -0600

    Finished up homepage

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api/index.js');
+const homeRoutes = require('./home-routes.js');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 // If a request is made to an endpoint that doesn't exist....
 router.use((req, res) => {
